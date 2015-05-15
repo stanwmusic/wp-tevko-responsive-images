cd373e3e092b1ab2c71c4dad94826fdf9abb98ac
Joe McGill <joemcgill@gmail.com>
2015-05-15 10:17:40 -0500

    convert spaces to tabs

--- js/wp-tevko-responsive-images.js
+++ js/wp-tevko-responsive-images.js
@@ -2,56 +2,56 @@
 
 (function() {
 
-  /**
-   * Recalculate srcset attribute after an image-update event
-   */
-  if ( wp.media ) {
-    wp.media.events.on( 'editor:image-update', function( args ) {
-      // arguments[0] = { Editor, image, metadata }
-  	  var image = args.image,
-  			metadata = args.metadata;
+	/**
+	 * Recalculate srcset attribute after an image-update event
+	 */
+	if ( wp.media ) {
+		wp.media.events.on( 'editor:image-update', function( args ) {
+			// arguments[0] = { Editor, image, metadata }
+			var image = args.image,
+				metadata = args.metadata;
 
-      // if the image url has changed, recalculate srcset attributes
-      if ( metadata && metadata.url !== metadata.originalUrl ) {
-        // we need to get the postdata for the image because
-        // the sizes array isn't passed into the editor
+			// if the image url has changed, recalculate srcset attributes
+			if ( metadata && metadata.url !== metadata.originalUrl ) {
+				// we need to get the postdata for the image because
+				// the sizes array isn't passed into the editor
 
-        // Update the srcset attribute
-        updateSrcset( image, metadata );
+				// Update the srcset attribute
+				updateSrcset( image, metadata );
 
-        // Update the sizes attribute
-        updateSizes( image, metadata );
-      }
+				// Update the sizes attribute
+				updateSizes( image, metadata );
+			}
 
-    });
-  }
+		});
+	}
 
-  /**
-   * Update the srcet attribute on an image in the editor
-   */
-  var updateSrcset = function( image, metadata ) {
+	/**
+	 * Update the srcet attribute on an image in the editor
+	 */
+	var updateSrcset = function( image, metadata ) {
 
-    var data = {
-      action: 'tevkori_ajax_srcset',
-      postID: metadata.attachment_id,
-      size: metadata.size
-    };
+		var data = {
+			action: 'tevkori_ajax_srcset',
+			postID: metadata.attachment_id,
+			size: metadata.size
+		};
 
-    jQuery.post( ajaxurl, data, function( response ) {
-      image.setAttribute( 'srcset', response );
-    });
-  }
+		jQuery.post( ajaxurl, data, function( response ) {
+			image.setAttribute( 'srcset', response );
+		});
+	}
 
-  /**
-   * Update the data-sizes attribute on an image in the editor
-   */
-  var updateSizes = function( image, metadata ) {
-    
-    var sizes = '(max-width: ' + metadata.width + 'px) 100vw, ' + metadata.width + 'px';
+	/**
+	 * Update the data-sizes attribute on an image in the editor
+	 */
+	var updateSizes = function( image, metadata ) {
 
-    // update the sizes attribute of our image
-    image.setAttribute( 'data-sizes', sizes );
-  }
+		var sizes = '(max-width: ' + metadata.width + 'px) 100vw, ' + metadata.width + 'px';
+
+		// update the sizes attribute of our image
+		image.setAttribute( 'data-sizes', sizes );
+	}
 
 
 })();
