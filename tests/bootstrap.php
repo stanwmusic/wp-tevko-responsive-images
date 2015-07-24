58d1b3903712171bc2d29ca4904d3175628bf641
Scott Taylor <scott.c.taylor@mac.com>
2015-07-24 15:00:09 -0400

    Fix unit test loading

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,28 @@
 <?php
+$_tests_dir = false;
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+// ../tests ../{plugin} ../plugins
+$dir = dirname( __FILE__ );
+
+if ( 'plugins' === basename( dirname( dirname( $dir ) ) ) ) {
+	$plugins = dirname( dirname( $dir ) );
+	// wordpress core svn = ../wp-content ../src
+	$step = dirname( dirname( $plugins ) );
+	$tests = '/tests/phpunit';
+	if ( 'src' === basename( $step ) ) {
+		$_tests_dir = dirname( $step ) . $tests;
+	// wordpress git = ../wp-content ../{root}
+	} elseif ( is_dir( $step . '/tests' ) ) {
+		$_tests_dir = $step . $tests;
+	}
+}
+
+if ( ! $_tests_dir ) {
+	$_tests_dir = getenv( 'WP_TESTS_DIR' );
+	if ( ! $_tests_dir ) {
+		$_tests_dir = '/tmp/wordpress-tests-lib';
+	}
+}
 
 require_once( $_tests_dir . '/includes/functions.php' );
 
